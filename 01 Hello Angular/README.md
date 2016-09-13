b640c2932a799da5e8701e243ad2d6d1ab377fa9
nasdan <daniel.sanchez@lemoncode.net>
2016-09-13 21:44:15 +0200

    Added Hello Angular project, missing explain sample in Readme

--- 01 Hello Angular/README.md
+++ 01 Hello Angular/README.md
@@ -1,1 +1,18 @@
 # 01 Hello Angular
+
+In this sample we are going to setup the basic to work with Angular.
+
+## Required dependencies
+
+- core-js
+- reflect-metadata
+- zone.js
+- @angular/core
+- @angular/platform-browser
+- @angular/platform-browser-dynamic
+
+## Peer dependencies
+
+- @angular/common
+- @angular/compiler
+- rxjs
---
+++ 01 Hello Angular/src/index.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { App } from './app';
+
+@NgModule({
+  declarations: [App],
+  imports: [BrowserModule],
+  bootstrap: [App]
+})
+class AppModule {
+
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
