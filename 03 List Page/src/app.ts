348b7a852937c886cbd7cf033cf13a11d6f53f72
Jaime Salas <jaime.salas@lemoncode.net>
2016-09-14 16:21:12 +0200

    Copy files from 01 Hello Angular

---
+++ 03 List Page/src/app.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component(
+  {
+    selector: 'app',
+    template: `
+      <h1>03 List page</h1>
+    `
+  }
+)
+class App {
+
+}
+
+export {
+  App
+}
