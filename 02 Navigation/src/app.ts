c2f81ab768ecab923dadadf4ea5a554e08d61174
nasdan <daniel.sanchez@lemoncode.net>
2016-09-16 17:03:32 +0200

    Copy files from 01 Hello Angular

---
+++ 02 Navigation/src/app.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component(
+  {
+    selector: 'app',
+    template: `
+      <h1>01 Hello angular</h1>
+    `
+  }
+)
+class App {
+
+}
+
+export {
+  App
+}
