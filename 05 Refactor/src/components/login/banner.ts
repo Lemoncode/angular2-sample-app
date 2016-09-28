1ec99e006805ed336577f801e31f86d26d2fc959
Braulio <braulio.diez@gmail.com>
2016-09-28 14:03:18 +0100

    Refactor Sample

---
+++ 05 Refactor/src/components/login/banner.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+const imageSrc = require('../../images/health.png');
+
+@Component({
+  selector: 'banner',
+  template: `
+  <div class="banner row hidden-xs">
+    <img src={{this.imageSrc}} class="img-responsive"/>
+  </div>
+  `
+})
+class Banner {
+  imageSrc: any;
+
+  constructor() {
+    this.imageSrc = imageSrc;
+  }
+}
+
+export {
+  Banner
+}
