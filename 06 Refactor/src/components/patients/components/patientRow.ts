04764e3fe387f225c1588056e3eab29933df6604
David Atencia <david.atencia@gmail.com>
2016-10-01 11:32:08 +0200

    Update source code

---
+++ 06 Refactor/src/components/patients/components/patientRow.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from "@angular/core";
+
+import { Patient } from "../../../model/patient";
+
+@Component({
+  selector: "[patient-row]",
+  template: `
+      <td class="hidden-xs hidden-sm hidden-md">{{patient.dni}}</td>
+      <td>{{patient.name}}</td>
+      <td>
+        {{patient.specialty}}
+        <span class="hidden-sm hidden-md hidden-lg pull-right glyphicon glyphicon-pencil"
+          [routerLink]="['/patient']">
+        </span>
+      </td>
+      <td class="hidden-xs hidden-sm hidden-md">{{patient.doctor}}</td>
+      <td class="hidden-xs">{{patient.date}}</td>
+      <td class="hidden-xs">
+        {{patient.time}}
+        <span class="pull-right glyphicon glyphicon-pencil"
+          [routerLink]="['/patient', patient.id]">
+        </span>
+      </td>
+  `
+})
+class PatientRow {
+  @Input("patient-row") patient: Patient;
+}
+
+export {
+  PatientRow
+}
